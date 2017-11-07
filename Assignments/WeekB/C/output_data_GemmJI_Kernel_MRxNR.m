895104aaaeaba580fd37ad1d4ad9a266bea24696
Mfam22 <m.martinez1120@gmail.com>
2017-11-07 16:19:30 -0600

    Week B

---
+++ Assignments/WeekB/C/output_data_GemmJI_Kernel_MRxNR.m
@@ -0,0 +1,24 @@
+% number of repeats:% 3
+% enter first, last, inc:% 128 1500 128 
+data_ref( 1, 1:2 ) = [ 128 3.347780e-04 ];
+data_GemmJI_Kernel_MRxNR( 1, 1:3 ) = [ 128 4.748480e-04 2.842171e-14];
+data_ref( 2, 1:2 ) = [ 256 2.116441e-03 ];
+data_GemmJI_Kernel_MRxNR( 2, 1:3 ) = [ 256 4.732677e-03 5.684342e-14];
+data_ref( 3, 1:2 ) = [ 384 5.297694e-03 ];
+data_GemmJI_Kernel_MRxNR( 3, 1:3 ) = [ 384 1.226072e-02 2.273737e-13];
+data_ref( 4, 1:2 ) = [ 512 7.721694e-03 ];
+data_GemmJI_Kernel_MRxNR( 4, 1:3 ) = [ 512 2.371175e-02 3.694822e-13];
+data_ref( 5, 1:2 ) = [ 640 1.460008e-02 ];
+data_GemmJI_Kernel_MRxNR( 5, 1:3 ) = [ 640 4.762538e-02 6.252776e-13];
+data_ref( 6, 1:2 ) = [ 768 2.426492e-02 ];
+data_GemmJI_Kernel_MRxNR( 6, 1:3 ) = [ 768 8.099935e-02 7.389644e-13];
+data_ref( 7, 1:2 ) = [ 896 3.919176e-02 ];
+data_GemmJI_Kernel_MRxNR( 7, 1:3 ) = [ 896 1.299113e-01 8.526513e-13];
+data_ref( 8, 1:2 ) = [ 1024 5.976444e-02 ];
+data_GemmJI_Kernel_MRxNR( 8, 1:3 ) = [ 1024 1.962306e-01 1.136868e-12];
+data_ref( 9, 1:2 ) = [ 1152 8.252722e-02 ];
+data_GemmJI_Kernel_MRxNR( 9, 1:3 ) = [ 1152 2.767885e-01 1.477929e-12];
+data_ref( 10, 1:2 ) = [ 1280 1.130273e-01 ];
+data_GemmJI_Kernel_MRxNR( 10, 1:3 ) = [ 1280 3.809207e-01 1.875833e-12];
+data_ref( 11, 1:2 ) = [ 1408 1.495715e-01 ];
+data_GemmJI_Kernel_MRxNR( 11, 1:3 ) = [ 1408 5.031611e-01 1.932676e-12];
